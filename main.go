c787d4e98924744d0def6e159dc8478649c85174
Aam Surganda <surganda@gmail.com>
2017-01-05 10:59:10 +0700

    belajar array dan slice

--- main.go
+++ main.go
@@ -2,40 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"reflect"
-	"strconv"
-	"github.com/aamsur/latihan-git/resources"
 )
 
 func main() {
-	var n int
+	var fruits = []string{"apple", "grape", "banana"}
+	var bFruits = []string{}
 
-	a := os.Args[1:]
+	bFruits = append(bFruits, fruits...)
 
-	var x = 214748364721
-	var y = 123
-	hasil1, _ := resources.Qasico(123, 321)
+	bFruits[0] = "buah"
 
-	//reflection
-	fmt.Println(reflect.TypeOf(x), x + y, hasil1)
-
-	n, _ = strconv.Atoi(a[0]);
-
-	first := 0
-	second := 1
-	next := 0
-	c := 0
-
-	for c = 0; c < n; c++ {
-		if c <= 1 {
-			next = c;
-		} else {
-			next = first + second;
-			first = second;
-			second = next;
-		}
-
-		fmt.Println(next)
-	}
+	fmt.Println(fruits)
+	fmt.Println(bFruits)
 }
